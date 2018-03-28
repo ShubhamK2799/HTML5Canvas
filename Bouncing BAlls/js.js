a8365c266e2fc7b44c0e215c05bc8b83534d6de6
Shubham Kumar <31592194+ShubhamK2799@users.noreply.github.com>
2018-03-28 23:32:21 +0530

    Add files via upload

---
+++ Bouncing BAlls/js.js
@@ -0,0 +1,91 @@
+var canvas=document.getElementById("myCanvas");
+var pen=canvas.getContext('2d');
+var nmax= 600; //maximum number of circles in max window width
+			//the number of circles decreases with the window size
+
+var n=nmax;
+var h=canvas.height=window.innerHeight;
+var w=canvas.width=window.innerWidth; //Used to increase the quality var rmin=10;
+var rmin=10;    //min circle rad possible
+var rmax=80;    //max circle rad possible
+var v=3;        //maximum moving velocity
+var mc_dis=50;  //mouse effect range
+var rate=1.5;   //radius increasing/decreasing speed
+
+//of circles
+var x =[n];
+var vx=[n];
+var vy=[n];
+var y =[n];
+var c =[n];
+var r =[n];
+
+var m ={x:undefined,y:undefined};
+
+
+//Adding event listners
+//element.addEventListner ("type", func(event));
+window.addEventListener("mousemove",function(event){
+		m.x=event.x; 
+		m.y=event.y;//use console.log ot see details
+		}
+	);
+
+
+
+function drawCircle(X,Y,Color,R){
+   	pen.beginPath();//start drawing
+   	pen.arc(X,Y,R,0,2*Math.PI);
+   	pen.fillStyle=Color;
+   	pen.fill();
+   	//pen.stroke();add stroke 
+}
+
+
+function start(){
+    n=Math.floor(nmax * (w/1260));
+  	for(var i=0; i<n;i++){
+    	x[i] = Math.random()*w;
+    	y[i] = Math.random()*h;
+    	vx[i]= Math.random()*v;
+    	vy[i]= Math.random()*v;
+    	r[i] = rmin;
+    	switch(Math.floor(5*Math.random())){
+    		case 0: c[i]= "#9C9"; break;
+    		case 1: c[i]= "#555"; break;
+    		case 2: c[i]= "#993"; break;
+    		case 3: c[i]= "#989"; break;
+    		case 4: c[i]= "#667"; 
+    	}
+    //No use of ; even after switch statement 
+  	}
+}
+
+
+function update(){
+ 
+   // console.log(n);
+    pen.clearRect(0,0,w,h);
+ 	for(var i=0; i<n;i++){
+ 		if(x[i]>w || x[i]<0)
+ 			vx[i]*=(-1);
+ 		if(y[i]>h || y[i]<0)
+ 			vy[i]*=(-1);
+ 		if(x[i] -m.x < mc_dis && x[i] -m.x >-mc_dis && y[i] -m.y < mc_dis && y[i] -m.y >-mc_dis)
+ 			r[i]=(r[i]>rmax)?rmax:r[i]+rate;
+ 		else
+ 			r[i]=(r[i]>rmin)?r[i]-rate:rmin;
+ 		 
+        drawCircle(x[i]+=vx[i],y[i]+=vy[i],c[i],r[i]);
+    }
+
+    //console.log(m.x,m.y);
+
+	window.requestAnimationFrame(update);
+}
+
+
+start();
+update();
+
+
---
+++ Rings at mouse/js.js
@@ -0,0 +1,104 @@
+var canvas=document.getElementById("myCanvas");
+var pen=canvas.getContext('2d');
+
+
+var h    = canvas.height=window.innerHeight;
+var w    = canvas.width =window.innerWidth;
+var n    = 25;
+var rMax = 30;
+var drMax= 2 ;
+var range= 50;
+var vMax = 2;
+
+
+var x  = [n];
+var y  = [n];
+var r  = [n];
+var vx = [n];
+var vy = [n];
+var dr = [n];
+var col= [n];
+
+var m ={x:undefined,y:undefined};//although circle and obj are same but they are two different variables and there is no concept of classes in js
+
+
+//Adding event listners
+//element.addEventListner ("type", func(event));
+window.addEventListener("mousemove",function(event){
+		m.x=event.x; 
+		m.y=event.y;//use console.log ot see details
+		}
+	);
+
+
+
+function drawCircleWrtM(i){
+   	pen.beginPath();//start drawing
+   	pen.arc(x[i] ,y[i],r[i],0,2*Math.PI);
+   	pen.strokeStyle=col[i];
+    pen.stroke();
+    console.log(x[i],y[i]);
+}
+
+function init(i){
+	if(m==undefined){
+		x[i] = w/2; //+ range*(1-2*Math.random());
+    	y[i]= h/2; //range*(1-2*Math.random());
+    }
+    else{
+    	x[i]=m.x;
+    	y[i]=m.y;
+    }	
+    vx[i]= vMax*(1-2*Math.random());
+    vy[i]= vMax*(1-2*Math.random());
+    r[i]= rMax*Math.random();
+    dr[i]= drMax*(1-2*Math.random()); 
+    switch(Math.floor(5*Math.random())){
+    	case 0 :col[i]= "#9C9"; break;
+    	case 1 :col[i]= "#555"; break;
+    	case 2 :col[i]= "#993"; break;
+    	case 3 :col[i]= "#989"; break;
+    	default:col[i]= "#667"; 
+    };	
+    //c[i] = circle; they alll will point to same memory location although the circle.x is refering to different points st each call but unitll
+    //we are not creating a new variable each time all the memebers are having their root point at circle which is not changing 
+}
+
+function start(){
+    for(var i=0; i<n;i++)
+      init(i);  
+}
+
+
+function update(){
+  
+  pen.clearRect(0,0,w,h);
+  for(var i=0; i<n;i++){
+    
+    x[i]+=vx[i];
+    y[i]+=vy[i];
+    r[i]+=dr[i];
+    if((x[i] > w+r[i]  || x[i]<-r[i]) || (y[i]> h+r[i]  || y[i]<-r[i]) || r[i]<0.5)
+       init(i); 
+
+    if(r[i]>rMax) dr[i]*=(-1);
+
+    drawCircleWrtM(i);
+  }
+  //If i would have considered the region to be a circle then the reflection and detection would have been difficult
+  //console.log(c);
+	window.requestAnimationFrame(update);
+}
+
+
+start();
+update();
+
+/*
+I had used here an array of circles where the circles were an object of type c{x,y,vx...}
+When i initialied a circle and changed the memebers and after that copied the value to the c[i] all the 
+circles were refering to the same address.
+So i better created an array of individual axes.
+Actually it is better because using x[i[ is better thn useing s[i].x
+*/
+
